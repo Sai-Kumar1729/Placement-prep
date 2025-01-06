3b1a55eac60b976f0a46f269adba8baf3d68b40e
SAI KUMAR <110832141+Sai-Kumar1729@users.noreply.github.com>
2025-01-06 23:06:50 +0530

    Create Single_number.py

---
+++ Striver/Arrays/Single_number.py
@@ -0,0 +1,13 @@
+class Solution:​
+
+    def singleNumber(self, nums: List[int]) -> int:​
+
+        result=0​
+
+        for i in nums:​
+
+            result=result^i​
+
+        return result​
+
+​
